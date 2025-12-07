ca294bd925ae040959ccfd58bd43a692bb95e0c0
Nursima Betül BALLİ <nursimabetulballi@gmail.com>
2025-12-07 21:45:25 +0300

    App User ve Randevu tanımlamaları

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FitnessCenterReservationSystem.Models
+{
+	public class ApplicationUser:IdentityUser
+	{
+		public ICollection<Randevu>?Randevular{ get; set; }
+	}
+}
---
+++ Models/Randevu.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessCenterReservationSystem.Models
+{
+	public class Randevu
+	{
+		public int RandevuId { get; set; }
+		public DateTime Tarih { get; set; }
+		public string? ApplicationUserId { get; set; } //Foreign key
+		public ApplicationUser? ApplicationUser { get; set; }   //Navigation property
+
+	}
+}
