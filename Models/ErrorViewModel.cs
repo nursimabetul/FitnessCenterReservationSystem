2c3b207b610617cbfcaa5845d5d362b9626b8a67
Nursima Betül BALLİ <nursimabetulballi@gmail.com>
2025-12-07 19:28:20 +0300

    Proje dosyası ekle.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitnessCenterReservationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
