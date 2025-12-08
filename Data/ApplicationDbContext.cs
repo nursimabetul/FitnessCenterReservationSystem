1e96f03d977b9825c9b4f443122fe77a960661e3
Nursima Betül BALLİ <nursimabetulballi@gmail.com>
2025-12-08 20:46:04 +0300

    App User Düzenlendi

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FitnessCenterReservationSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FitnessCenterReservationSystem.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
